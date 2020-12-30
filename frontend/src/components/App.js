8983d50b82c7008e5b07f854934d66affff77ed3
angelopoole <angelo.poole@outlook.com>
2020-12-29 21:28:34 -0500

    testing husky/lintStaged

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,22 @@
 //libs
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 //components
-import Layout from './Layout';
-import HomeScreen from '../pages/HomeScreen';
-import ChampionDetailScreen from '../pages/ChampionDetailScreen';
+import Layout from './Layout'
+import HomeScreen from '../pages/HomeScreen'
+import ChampionDetailScreen from '../pages/ChampionDetailScreen'
 
 function App() {
-	return (
-		<Router>
-			<Layout>
-				<Route path='/' component={HomeScreen} exact />
-				<Route path='/champion/:id' component={ChampionDetailScreen} />
-			</Layout>
-		</Router>
-	);
+  return (
+    <Router>
+      <Layout>
+        <Route path="/" component={HomeScreen} exact />
+        <Route path="/champion/:id" component={ChampionDetailScreen} />
+      </Layout>
+    </Router>
+  )
 }
 
 /* <Route path='/order/:id' component={OrderScreen} /> */
 
-export default App;
+export default App
