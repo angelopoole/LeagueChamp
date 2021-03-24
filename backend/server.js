fd17b57f3965b5bc12bc61489014c3cf384d233f
angelopoole <angelo.poole@outlook.com>
2021-03-24 10:12:02 -0400

    added dotenv, setting up for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import colors from 'colors';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 // import { imageDownloader } from './util/photoDownloader.js';
 // image downloader will download the static assets for every champion base splash art
 
@@ -12,6 +17,20 @@ app.use(express.json());
 app.use('/api/datadragon/champions', dataDragonRoutes);
 
 // imageDownloader();
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
